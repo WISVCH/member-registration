e8e1f35fbfc19b1a024adc0c53ee2c5948479164
Julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2021-05-01 13:15:20 +0200

    add repo

---
+++ backend/src/main/kotlin/com/wisv/ch/memberregistration/member/service/MemberRepository.kt
@@ -0,0 +1,13 @@
+package com.wisv.ch.memberregistration.member.service
+
+import com.wisv.ch.memberregistration.member.model.Member
+import com.wisv.ch.memberregistration.paidstatus.model.PaidStatus
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MemberRepository : JpaRepository<Member, Long>{
+	fun findAllByPaidStatus(paidStatus: PaidStatus): List<Member>
+
+	fun findAllByAddedToLdb(addedToLdb: Boolean): List<Member>
+}
